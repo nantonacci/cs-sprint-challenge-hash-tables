3aa01182f83f6af389633f4781e0d50f464d9b55
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-07-19 18:42:32 -0700

    Exercise 4 completed

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,24 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+
+    for i in a:
+        if i > 0:
+            cache[i] = 0
+    
+    for x in a:
+        if x < 0:
+            pos = -x
+            if pos not in cache:
+                pass
+            else:
+                cache[pos] += 1
+
+    for item in cache:
+        if cache[item] > 0:
+            result.append(item)
 
     return result
 
