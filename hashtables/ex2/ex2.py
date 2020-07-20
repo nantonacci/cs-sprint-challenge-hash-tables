4aace86485326eaa7f054e15d65dd2c556b2b9c9
Nicole Antonacci <nicole.antonacci@gmail.com>
2020-07-19 18:18:29 -0700

    Exercise 2 completed.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,20 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    route = [None] * length
+
+    for i in range(length):
+
+        if tickets[i].source is "NONE":
+            route[0] = tickets[i].destination
+
+        if tickets[i].source not in cache:
+            cache[tickets[i].source] = tickets[i].destination
+
+    for x in range(length):
+        
+        if route[x - 1] is not None:
+            route[x] = cache[route[x - 1]]
 
     return route
